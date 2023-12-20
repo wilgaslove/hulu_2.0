d0f7a542aca6989cdc174ea4e0313cbf469f2b4a
wilgaslove <wilfried216@gmail.com>
2023-12-20 18:12:40 +0100

    finalisation de component

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -1,15 +1,14 @@
 <template>
-      <li class="flex flex-col items-center cursor-pointer w-20 sm:w-20 hover:text-white group">
-            <Icon class="h-8 mb-1 group-hover:animate-bounce" />
-            <a href="#" class="uppercase">{{ name }}</a>
-        </li>
-        
+  <li class="flex flex-col items-center cursor-pointer w-20 sm:w-20 hover:text-white group">
+    <Icon class="h-8 mb-1 group-hover:animate-bounce" />
+    <a href="#" class="uppercase">{{ name }}</a>
+  </li>
 </template>
 
 <script setup lang="ts">
-import type { FunctionalComponent, HTMLAttributes, VNodeProps } from "vue";
+import type { FunctionalComponent, HTMLAttributes, VNodeProps } from 'vue'
 const props = defineProps<{
-    name: String,
-    Icon: FunctionalComponent<HTMLAttributes & VNodeProps, {}, any>,
-}>();
+  name: String
+  Icon: FunctionalComponent<HTMLAttributes & VNodeProps, {}, any>
+}>()
 </script>
