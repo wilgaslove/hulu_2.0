87e5c46fbc1929a37e092e6fcf4d8567135c2b47
wilgaslove <wilfried216@gmail.com>
2023-12-20 18:09:55 +0100

    ajout de heroicon

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="flex flex-col sm:flex-row m-5 justify-between items-center">
+      <ul class="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem v-for="link of links"
+        :key="link.name"
+         :name="link.name" 
+         :Icon="link.icon" />
+        
+       
+      </ul>
+      <img src="../assets/ua6.webp" alt="" srcset="">
+    </header>
+</template>
+<script setup lang="ts">
+import HeaderItem from "@/components/HeaderItem.vue";
+import { 
+HomeIcon,
+BoltIcon,
+CheckBadgeIcon,
+RectangleStackIcon,
+MagnifyingGlassIcon,
+UserIcon,
+ } from "@heroicons/vue/24/outline";
+ const links = [
+    {name: "home", icon: HomeIcon},
+    {name: "trending", icon: BoltIcon},
+    {name: "verified", icon: CheckBadgeIcon},
+    {name: "collections", icon: RectangleStackIcon},
+    {name: "search", icon: MagnifyingGlassIcon},
+    {name: "account", icon: UserIcon},
+ ]
+</script>
